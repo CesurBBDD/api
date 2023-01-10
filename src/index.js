90bb4e7871a200033a9d5a8a351c562f8b4e1c7d
bernat <bernat13@gmail.com>
2023-01-10 07:08:32 +0100

    dockerfile

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 var express = require('express') //llamamos a Express
 var app = express()       
-var bodyParser = require('body-parser')        
+//var bodyParser = require('body-parser')        
 
 var port = process.env.PORT || 4000  // establecemos nuestro puerto
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())            
+//app.use(bodyParser.urlencoded({ extended: true }))
+//app.use(bodyParser.json())            
 
 // nuestra ruta irá en http://localhost:8080/api
 // es bueno que haya un prefijo, sobre todo por el tema de versiones de la API
